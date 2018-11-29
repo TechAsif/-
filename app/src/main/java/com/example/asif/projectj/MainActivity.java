0ff74d36131288364889bac1f2cdaca85aa08a20
Äsif parvez <asifparvezsarker26@gmail.com>
2018-11-29 10:17:59 +0600

    first commit main menu done

---
+++ app/src/main/java/com/example/asif/projectj/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.asif.projectj;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mainButtonOne,mainButtonTwo,mainButtonThree,mainButtonFour,mainButtonFive,mainButtonSix;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Button initialize
+        mainButtonOne=findViewById(R.id.main_button_one);
+        mainButtonTwo=findViewById(R.id.main_button_two);
+        mainButtonThree=findViewById(R.id.main_button_three);
+        mainButtonFour=findViewById(R.id.main_button_four);
+        mainButtonFive=findViewById(R.id.main_button_five);
+        mainButtonSix=findViewById(R.id.main_button_six);
+
+
+
+        mainButtonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainButtonOneActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mainButtonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(MainActivity.this,MainButtonTwoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mainButtonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(MainActivity.this,MainButtonThreeActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        mainButtonFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainButtonFourActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        mainButtonFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainButtonFiveActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        mainButtonSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(MainActivity.this,MainButttonSixActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/asif/projectj/MainButtonFiveActivity.java
@@ -0,0 +1,13 @@
+package com.example.asif.projectj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainButtonFiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_button_five);
+    }
+}
